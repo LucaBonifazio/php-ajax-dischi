eedd5de4d3d910a8720a69103104e2f1feb31dea
LucaBonifazio <luca.bonifazio13@gmail.com>
2022-12-16 16:36:53 +0100

    Complete without bonus

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+include_once __DIR__ . '/db/data.php';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -14,5 +18,61 @@
     <script src="assets/script.js" defer></script>
 </head>
 <body>
+    <div id="app">
+        <header>
+            <div class="top_bar d-flex justify-content-between align-items-center px-4">
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/2048px-Spotify_logo_without_text.svg.png"
+                alt="logo"
+                class="img-fluid"
+              >
+              <!-- <div>
+                <label for="select-genres">
+                  <select
+                    id="select-genres"
+                    v-model="genreFilter"
+                    name="genre"
+                    @change="genreChange"
+                  >
+                    <option value="all">
+                      All
+                    </option>
+                    <option
+                      v-for="genre in genresList"
+                      :key="genre"
+                      :value="genre"
+                    >
+                      {{ genre }}
+                    </option>
+                  </select>
+                </label>
+              </div> -->
+            </div>
+        </header>
+        <main class="d-flex p-1">
+            <div class="row row-cols-sm-2 row-cols-md-3 row-cols-lg-5 g-3 container">
+            <?php foreach ($arrAlbums as $value) { ?>
+                <div class="col">
+                    <div class="artist_card text-center">
+                        <img
+                        src="<?= $value['poster'] ?>"
+                        alt="<?= $value['title'] ?>"
+                        class="img-fluid rounded p-2"
+                        >
+                        <h2 class="title p-2">
+                            <?= $value['title'] ?>
+                        </h2>
+                        <div class="author">
+                        <?= $value['author'] ?>
+                        </div>
+                        <div class="year">
+                        <?= $value['year'] ?>
+                        </div>
+                    </div>
+                </div>
+            <?} ?>
+            </div>
+        </main>
+    </div>
 </body>
 </html>
